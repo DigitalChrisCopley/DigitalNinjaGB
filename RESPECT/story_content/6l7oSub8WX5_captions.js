62c09202af047372158da23d2077797efed06e92
DigitalChrisCopley <49977345+DigitalChrisCopley@users.noreply.github.com>
2023-03-13 12:43:15 +0000

    Add RESPECT Files

---
+++ RESPECT/story_content/6l7oSub8WX5_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.61.27106.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:04.545%0D%0AHow%20can%20we%20do%20this?%0D%0AClick%20the%20white%20box%20or%20press%20enter%20for%20the%20answer.%0D%0A%0D%0A"}')
